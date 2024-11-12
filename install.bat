b0380f0eaf72616beaeee34907ea6661cadb4d62
Diego Bellani <bellanidiego@gmail.com>
2024-11-12 15:02:47 +0100

    I don't even know

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 rem https://tex.stackexchange.com/a/1141
 FOR /F "tokens=* USEBACKQ" %%F IN (`kpsewhich -var-value TEXMFLOCAL`) DO (SET dest=%%F)
-COPY /Y matrixcalculus.sty "%dest%"
+COPY /Y matrixcalculus.sty "%dest%\tex\latex\local"
 texhash
