d4c04f8579364ffa55b41e787d3aca7f8af10e45
Diego Bellani <bellanidiego@gmail.com>
2025-03-25 11:05:01 +0100

    Small improvements

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
+@echo off
 rem https://tex.stackexchange.com/a/1141
 FOR /F "tokens=* USEBACKQ" %%F IN (`kpsewhich -var-value TEXMFLOCAL`) DO (SET dest=%%F)
 COPY /Y matrixcalculus.sty "%dest%\tex\latex\local"
