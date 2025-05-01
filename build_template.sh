6a3ae81c57e8a31d1f267332f41e815050ed8fdb
Diego Bellani <bellanidiego@gmail.com>
2025-05-01 15:30:13 +0200

    Improved diagnostics for shell script

--- build_template.sh
+++ build_template.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-set -e
-
 flags="-interaction batchmode -file-line-error -halt-on-error"
 fname="document"
 
-mpost    $flags figures
-pdflatex $flags "$fname"
-bibtex          "$fname"
-pdflatex $flags "$fname"
-pdflatex $flags "$fname"
+mpost    $flags figures  ; err=$? ; cat figures.log    ; if [ $err -ne 0 ] ; then exit $err; fi
+pdflatex $flags "$fname" ; err=$? ; cat "${fname}.log" ; if [ $err -ne 0 ] ; then exit $err; fi
+bibtex          "$fname" ; err=$? ; cat "${fname}.blg" ; if [ $err -ne 0 ] ; then exit $err; fi
+pdflatex $flags "$fname" ; err=$? ; cat "${fname}.log" ; if [ $err -ne 0 ] ; then exit $err; fi
+pdflatex $flags "$fname" ; err=$? ; cat "${fname}.log" ; if [ $err -ne 0 ] ; then exit $err; fi
