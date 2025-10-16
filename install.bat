faff3cfd67166650fd32c968311ab090820c1dff
Diego Bellani <bellanidiego@gmail.com>
2025-10-16 22:39:09 +0200

    Added a new package

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
 rem https://tex.stackexchange.com/a/1141
 FOR /F "tokens=* USEBACKQ" %%F IN (`kpsewhich -var-value TEXMFLOCAL`) DO (SET dest=%%F)
-COPY /Y matrixcalculus.sty "%dest%\tex\latex\local"
+COPY /Y *.sty "%dest%\tex\latex\local"
 texhash
--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 dest=`kpsewhich -var-value=TEXMFHOME`
 dest="${dest}/tex/latex/local"
 mkdir -p "$dest"
-cp -f matrixcalculus.sty "$dest"
+cp -f *.sty "$dest"
